675276445d8ebbfd220dda751c6de5cb71295588
puliwang@cxist.com <puliwang@cxist.com>
2021-05-18 11:34:05 +0800

    添加测试专用的json server

---
+++ db.js
@@ -0,0 +1,14 @@
+const faker = require('faker');
+
+module.exports = () => {
+    const data = { users: [], companies: [] }
+    // 创建一些初始数据
+    for (let i = 0; i < 200; i++) {
+        data.users.push({ id: faker.datatype.uuid(), name: faker.name.findName(), age: faker.datatype.number({ min: 5, max: 20 }) });
+    }
+    for (let i = 0; i < 200; i++) {
+        data.companies.push({ id: faker.datatype.uuid(), name: faker.company.companyName() });
+    }
+
+    return data;
+}
