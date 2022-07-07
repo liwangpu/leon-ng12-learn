bfffbd475d801e51340d276a08693b3476fc0828
puliwang <puliwang@exiao.tech>
2022-07-07 18:14:07 +0800

    1

--- src/app/tutorial/components/home/home.component.ts
+++ src/app/tutorial/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,16 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  apps: any[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    for (let idx = 20; idx >= 0; idx--) {
+      this.apps.push({
+        id: faker.datatype.uuid(),
+        title: `这是一个名字很长很长的应用测试专用看看会怎么截取`
+      });
+    }
   }
 
 }
