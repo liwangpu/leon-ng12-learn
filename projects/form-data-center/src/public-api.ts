3e533314447853faf5452b7665023ea4223a04ef
leon <puliwang@cxist.com>
2022-07-11 23:25:19 +0800

    1

---
+++ projects/form-data-center/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of form-data-center
+ */
+
+export * from './lib/form-data-center.service';
+export * from './lib/form-data-center.component';
+export * from './form-data-center.module';
--- src/app/dynamic-form-demo/dynamic-form-demo.module.ts
+++ src/app/dynamic-form-demo/dynamic-form-demo.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DynamicFormDemoRoutingModule } from './dynamic-form-demo-routing.module';
-import { FormModule as DynamicFormModule } from 'dynamic-form/form';
+import { FormModule as DynamicFormModule } from 'dynamic-form';
 import { FormDetailComponent } from './components/form-detail/form-detail.component';
 
 @NgModule({
     declarations: [
-    FormDetailComponent
-  ],
+        FormDetailComponent
+    ],
     imports: [
         CommonModule,
         DynamicFormDemoRoutingModule,
