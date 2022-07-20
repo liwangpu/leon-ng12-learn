94290b59be22dd345b4bb313f9ec58dcc156fa54
puliwang <puliwang@exiao.tech>
2022-07-20 16:46:58 +0800

    1

--- projects/form-data-center/src/form-data/reducer.ts
+++ projects/form-data-center/src/form-data/reducer.ts
@@ -2,9 +2,14 @@ import { ActionType, combineReducers, createReducer, on } from '@ngrx/store';
 import * as fromState from './state';
 import * as fromAction from './action';
 
-export const formDataReducer = createReducer(fromState.INITIAL_FORM__DATA_STATE,
-    on(fromAction.setFormData, (state: fromState.IFormDataState, { formKey, data }) => {
-        // console.log('sss:', state);
-        return { ...state, [formKey]: data };
-    })
-);
+// export const formDataReducer = createReducer(fromState.INITIAL_FORM__DATA_STATE,
+//     on(fromAction.setFormData, (state: fromState.IFormDataState, { formKey, data }) => {
+//         // console.log('sss:', state);
+//         return { ...state, [formKey]: data };
+//     })
+// );
+
+on<fromState.IFormDataState,fromAction.setFormData>(fromAction.setFormData, (state: fromState.IFormDataState, { formKey, data }) => {
+    // console.log('sss:', state);
+    return { ...state, [formKey]: data };
+})
